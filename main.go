a64a7fbdecc82205417d878470c0a04dc6e6a406
Cesa Satria Anwar <Cesa263@gmail.com>
2024-03-27 14:09:08 +0700

    Update main.go

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("GET /halo", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintln(w, "Haturnuhun onegaishimasu")
+		fmt.Fprintln(w, "Yang bener aja lu woe")
 	})
 	server := http.Server{
 		Addr:    ":4343",
